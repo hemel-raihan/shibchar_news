01e25f4bfd527f5fe53afc42de164a5751716d03
hemel-raihan <hemelraihan1997@gmail.com>
2022-08-03 17:50:16 +0600

    add style for front home page

--- components/frontend/Header.js
+++ components/frontend/Header.js
@@ -9,14 +9,14 @@ export default function Header({title}) {
     <>
     <Head>
         <title>Shibchar - {title}</title>
-        {/* <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Poppins:300,400,500,600,700|PT+Serif:400,400i&display=swap" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Poppins:300,400,500,600,700|PT+Serif:400,400i&display=swap" rel="stylesheet" type="text/css" />
         <link rel="stylesheet" href="/assets/frontend/css/bootstrap.css" type="text/css" />
         <link rel="stylesheet" href="/assets/frontend/css/style.css" type="text/css" />
         <link rel="stylesheet" href="/assets/frontend/css/dark.css" type="text/css" />
         <link rel="stylesheet" href="/assets/frontend/css/font-icons.css" type="text/css" />
         <link rel="stylesheet" href="/assets/frontend/css/animate.css" type="text/css" />
         <link rel="stylesheet" href="/assets/frontend/css/magnific-popup.css" type="text/css" />
-        <link rel="stylesheet" href="/assets/frontend/css/custom.css" type="text/css" /> */}
+        <link rel="stylesheet" href="/assets/frontend/css/custom.css" type="text/css" />
    </Head>
 
    <Script
